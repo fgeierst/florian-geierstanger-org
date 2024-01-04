b18f2f57940bcda717e35b42e1a5572198b28f07
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2024-01-04 20:15:24 +0100

    feat: enable vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,21 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: 'github-light'
-    }
+      theme: "github-light",
+    },
   },
-  site: 'https://florian.geierstanger.org',
-  integrations: [sitemap(), svelte(), mdx()]
+  site: "https://florian.geierstanger.org",
+  integrations: [sitemap(), svelte(), mdx()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
