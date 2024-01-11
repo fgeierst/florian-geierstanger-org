c68b604e6c3652d5a383dfd92e782765ac01fd13
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2024-01-11 13:37:54 +0100

    feat: translate ui strings

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+  en: "English",
+  de: "Deutsch",
+};
+
+export const defaultLang = "de";
+
+export const ui = {
+  en: {
+    "nav.home": "Homepage",
+    "nav.skip": "Skip to content",
+  },
+  de: {
+    "nav.home": "Startseite",
+    "nav.skip": "Zum Hauptinhalt springen",
+  },
+} as const;
